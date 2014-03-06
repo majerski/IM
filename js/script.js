c1b6d8eccabf4f1c424d2b9e18ac6ecc56ce7870
majerski <tomasz.majerski@o2.pl>
2014-03-06 15:57:04 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/*! waitForImages jQuery Plugin 2013-07-20 */
+!function(a){var b="waitForImages";a.waitForImages={hasImageProperties:["backgroundImage","listStyleImage","borderImage","borderCornerImage","cursor"]},a.expr[":"].uncached=function(b){if(!a(b).is('img[src!=""]'))return!1;var c=new Image;return c.src=b.src,!c.complete},a.fn.waitForImages=function(c,d,e){var f=0,g=0;if(a.isPlainObject(arguments[0])&&(e=arguments[0].waitForAll,d=arguments[0].each,c=arguments[0].finished),c=c||a.noop,d=d||a.noop,e=!!e,!a.isFunction(c)||!a.isFunction(d))throw new TypeError("An invalid callback was supplied.");return this.each(function(){var h=a(this),i=[],j=a.waitForImages.hasImageProperties||[],k=/url\(\s*(['"]?)(.*?)\1\s*\)/g;e?h.find("*").addBack().each(function(){var b=a(this);b.is("img:uncached")&&i.push({src:b.attr("src"),element:b[0]}),a.each(j,function(a,c){var d,e=b.css(c);if(!e)return!0;for(;d=k.exec(e);)i.push({src:d[2],element:b[0]})})}):h.find("img:uncached").each(function(){i.push({src:this.src,element:this})}),f=i.length,g=0,0===f&&c.call(h[0]),a.each(i,function(e,i){var j=new Image;a(j).on("load."+b+" error."+b,function(a){return g++,d.call(i.element,g,f,"load"==a.type),g==f?(c.call(h[0]),!1):void 0}),j.src=i.src})})}}(jQuery);
+
 $(document).on("pageload",function() {
 	initiateScripts();
 });
@@ -14,15 +17,8 @@ function initiateScripts(){
 	buildSwiper();
 }
 function buildSwiper(){
-	//var h = $(document).height() - ($("header").height() + $("#main_bar").height() + $("#sub_bar").height());
-	//$(".swiper-container").css({"min-height":h});
 	var loadedSlides = new Array();
 	var mySwiper = $('.swiper-container').swiper({
-		//calculateHeight:true,
-		//cssWidthAndHeight: true,
-		//watchActiveIndex: true,
-		freeMode:true,
-		freeModeFluid: true,
 		resizeReInit: true,
 		updateOnImagesReady: true,
 		onFirstInit: function(s){
@@ -33,19 +29,18 @@ function buildSwiper(){
 						mySwiper.reInit();
 						loadedSlides.push(slides[0]);
 						loadedSlides.push(slides[1]);
+						updateSwiperHeight();
 					});
 				});
 			} else {
 				$(".swiper-wrapper").load(slides[0],function(r2){
 					mySwiper.reInit();
 					loadedSlides.push(slides[0]);
+					updateSwiperHeight();
 				});
 			}
 			$('.slides_left').html(slides.length);
 			$(".swiper-wrapper, .swiper-slide").height(1);
-		},
-		onSwiperCreated:function(){
-			updateSwiperHeight();
 		},
 		onSlideNext: function(s){
 			if(typeof slides[mySwiper.activeIndex+1]!='undefined' && slides.length > loadedSlides.length){
@@ -68,19 +63,20 @@ function buildSwiper(){
 	$("#sub_bar .arrow-left").bind("click",function(){
 		mySwiper.swipePrev();
 	});
-	window.addEventListener( 'onorientationchange', updateSwiperHeight, false );
+	//window.addEventListener('orientationchange', updateSwiperHeight);
 	window.onresize = function(event) {
-		var h = $(document).height() - ($("header").height() + $("#main_bar").height() + $("#sub_bar").height());
-		$('.swiper-container').height(h);
 		mySwiper.reInit();
+		updateSwiperHeight();
 	};
 }
 function updateSwiperHeight(){
 	var totalHeight = 0;
-	$(".swiper-slide-active").children().each(function(){
-		totalHeight += $(this).outerHeight();
+	$('.swiper-slide-active').waitForImages(function() {
+		$(".swiper-slide-active").children().each(function(){
+			totalHeight += $(this).outerHeight();
+		});
+		$(".swiper-wrapper, .swiper-slide").height(totalHeight);
 	});
-	$(".swiper-wrapper, .swiper-slide").height(totalHeight);
 }
 function contentSwipeLeftHandler(event){
 	var position = $(".ui-page-active .content").hasClass("open-right") ? 'left' : ($(".ui-page-active .content").hasClass("open-left") ? 'right' : 'center');
